ddc7919dd52dddf15df7f5c39ca6f18bf6d59fd5
philemmons <pemmons@csumb.edu>
2020-05-17 20:10:40 -0700

    step 6: adding angular material

--- angular_tutorial/angular-example/src/app/app.module.ts
+++ angular_tutorial/angular-example/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// NEW
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, //New
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
