b7bc2d743088226e3aabc2d33bad9e3d042358a9
philemmons <pemmons@csumb.edu>
2020-05-18 19:39:40 -0700

    removed defualt html; added angular twitter link to header; tweaked twitter logo css; finished step 9

--- angular_tutorial/angular-example/src/app/api.service.ts
+++ angular_tutorial/angular-example/src/app/api.service.ts
@@ -1,17 +1,16 @@
 
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 import {  throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  private SERVER_URL = "http://localhost:3000/products";
+  private SERVER_URL = "http://localhost:3000/products/";
 
   constructor(private httpClient: HttpClient) { }
 
@@ -28,7 +27,8 @@ export class ApiService {
     return throwError(errorMessage);
   }
 
-  public sendGetRequest(){
-    return this.httpClient.get(this.SERVER_URL).pipe(catchError(this.handleError));
+  public get(){  
+	return this.httpClient.get(this.SERVER_URL).pipe(catchError(this.handleError));
   }
+
 }
